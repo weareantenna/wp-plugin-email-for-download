cf4ef615c80dc72ffdadfd41c8a9a60034e03580
Pieter Van Peteghem <pieter@weareantenna.be>
2020-03-24 23:25:45 +0100

    remove unused files

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-
-	if(!window.location.search.length) return;
-
-	if(window.location.search.match("efd_download").length){
-		$('#efd_force_download').submit();
-	}
-
-});
